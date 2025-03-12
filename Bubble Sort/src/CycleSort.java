ff2b6a5bb1571bf63c308de79c3c00070a4f9ad4
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-03-13 00:42:32 +0530

    CycleSort

---
+++ Bubble Sort/src/CycleSort.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static java.util.Collections.swap;
+
+public class CycleSort {
+    public static void main(String[] args){
+int [] arr={5,4,3,1,2};
+sort(arr);
+System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr){
+        int i=0;
+        while (i< arr.length){
+            int correctIndex=arr[i]-1;
+            if(arr[i]!=arr[correctIndex]){
+                swap(arr,i,correctIndex);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
