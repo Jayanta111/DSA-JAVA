722607abd8866a83765fdd198345c5d368291bb1
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-03-14 01:37:05 +0530

    Google interview question

---
+++ Bubble Sort/src/LeetCode448.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode448 {
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        List<Integer> result = findDisappearedNumbers(nums);
+        System.out.println("The Disappeared Numbers in an Array are: " + result);
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        // Find missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
