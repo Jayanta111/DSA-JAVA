da26046f23c318eca30fe852e7c659b0993e7111
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-03-18 01:13:48 +0530

    Amazon Interview question Medium Level

---
+++ Bubble Sort/src/leetCode41.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class leetCode41 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 0};
+        int ans = firstMissingPositive(nums);
+        System.out.println(ans); // Corrected to print the integer answer
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return index + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
