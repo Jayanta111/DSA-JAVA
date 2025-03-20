03f5850f1943228472ccd18206766e2c5d06e04f
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-03-20 23:48:24 +0530

    String

---
+++ String/src/Comparison.java
@@ -0,0 +1,9 @@
+public class Comparison {
+    public static void main(String[] args){
+        String a="Jayanta";
+        String b="Jayanta";
+String name1=new String("Jayanta");
+    String name2=new String("Jayanta");
+    System.out.println(name1.equals(name2));
+    }
+}
