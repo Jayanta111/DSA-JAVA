784e7318e36dfb98c684a6879730f40025f70a40
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-03-20 23:27:42 +0530

    String

---
+++ String/src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //
+        int[] arr={2,3,5,4,19};
+        String name="Jayanta Chungkrang";
+        System.out.printf(name);
+
+
+    }
+}
