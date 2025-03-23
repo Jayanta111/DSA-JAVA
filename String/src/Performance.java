469aeac669ab7b2d4bc0523df34d37902fb1f990
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-03-24 01:35:41 +0530

    Methods in Java

--- String/src/Performance.java
+++ String/src/Performance.java
@@ -2,10 +2,14 @@
 public class Performance {
 
     public static void main(String[] args) {
+        StringBuilder builder=new StringBuilder();
 for(int i=0;i<26;i++){
 char ch=(char)('a'+i);
-System.out.println(ch);
+builder.append(ch);
 
 }
+System.out.println(builder.toString());
+builder.delete(2, 26);
+
     }
 }
