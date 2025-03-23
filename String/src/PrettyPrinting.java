97f0686279ebebd7a521b5bdec712ac5bfa32d3f
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-03-24 01:14:00 +0530

    String build in alphabet

--- String/src/PrettyPrinting.java
+++ String/src/PrettyPrinting.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class PrettyPrinting {
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a=453.1254f;
+        System.out.printf("Formatted number is %.3f", a);
+        System.out.println();
+        System.out.println( Math.PI);
+        System.out.println('n'+'b');
+        System.out.println("a"+"b");
+        System.out.println((char) ('a'+ 3));
+
+
+
+    }
 }
