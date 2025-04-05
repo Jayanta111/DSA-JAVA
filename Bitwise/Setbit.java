7a6f0f9e85b4a901cd77b1e48e4667aa6add9288
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-04-06 00:41:49 +0530

    Setbit

---
+++ Bitwise/Setbit.java
@@ -0,0 +1,17 @@
+package Bitwise;
+
+public class Setbit {
+    public static void main(String[] args) {
+        int n=9;
+        int c=0;
+        while (n>0) {
+            if ((n &1)==1) {
+                c++;
+            }
+            n=n>>1;
+
+            
+        }
+        System.out.println(c);
+    }
+}
