fb3da880c428269a49a7ca0d68bda54318c541e2
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-04-06 01:11:30 +0530

    XOR

---
+++ Bitwise/XOR.java
@@ -0,0 +1,10 @@
+package Bitwise;
+
+public class XOR {
+    public static void main(String[] args) {
+        int a=3;
+        int b=9;
+    System.out.println("XOR of "+a+" and "+b+" is : "+(a^b));
+    System.out.println("XOR of "+a+" and "+b+" is : "+(a&b));   
+    }
+}
