b92d978a589016b198cf4aee174b0f0682316742
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-04-08 02:28:55 +0530

    Recursion

---
+++ DSA_Maths/factor.java
@@ -0,0 +1,15 @@
+package DSA_Maths;
+
+public class factor {
+    public static void main(String[] args) {
+        int n=20;
+        factor(n);
+    }
+    static void factor(int n){
+        for(int i=1;i<n;i++){
+if (n%i==0) {
+    System.out.println(i+ "");
+}
+        }
+    }
+}
