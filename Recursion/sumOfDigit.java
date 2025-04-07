465ebed1f848c44fc83ea81ce2c032bca9c848d2
Jayanta111 <Jayantachungkrang3@gmail.com>
2025-04-08 03:00:25 +0530

    Sum of Digit

---
+++ Recursion/sumOfDigit.java
@@ -0,0 +1,23 @@
+public class sumOfDigit {
+    public static void main(String[] args) {
+int n=1342;
+System.out.println(sum(n));
+
+    }
+  public  static int sum(int n){
+        if (n==0) {
+            return 1;
+        }
+return (n%10)*sum(n/10);
+    }
+public  static  int sum=0;
+    static int Reverse (int n){
+if (n==0) {
+    return n;
+}
+  
+    int rem=n%10;
+    sum=sum*10+rem;
+    return Reverse(n/10);
+}
+}
